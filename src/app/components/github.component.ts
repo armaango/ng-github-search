dc098ed79df05d3df65e7f2c7972234038062bea
armaango <armaan.goyal@gmail.com>
2017-03-02 03:26:57 -0800

    profile done

---
+++ src/app/components/github.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {GithubService} from '../services/github.service';
+@Component({
+    moduleId:module.id,
+  selector: 'github',
+  templateUrl: `github.component.html`,
+  providers:[GithubService]
+})
+export class GithubComponent  { 
+    user:any;
+    repos:any;
+
+    constructor(private _githubService:GithubService) {
+        console.log('Github Component init');
+
+        this._githubService.getUser().subscribe(user=>{
+            this.user = user;
+        });
+
+        this._githubService.getRepos().subscribe(repos=>{
+            this.repos = repos;
+        })
+    }
+ }
